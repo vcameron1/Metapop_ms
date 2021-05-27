5425af1e4cc5e98dd33472e18acd43e3d5906a61
vcameron1 <54558022+vcameron1@users.noreply.github.com>
2021-05-27 11:57:04 -0400

    change projection to 2 species metapop model

--- conceptual_fig/metapopEffect_fig.R
+++ conceptual_fig/metapopEffect_fig.R
@@ -11,18 +11,24 @@ png('../manuscript/img/concept_metapopEffect.png', width = 150, height = 150, un
 par(pty = "s",par(pty = "s", mar=c(4.5,4.5,1,0)))
 lwd <- 4
 
-# Define params
-E <- seq(0,1,le=100) # climatic suitability
-c <- 1-E # Colonization rate
-e <- 0.1/c # Extinction rate
+# Environmetal gradient
+seq <- seq(0.5, 1, le=20)
+
+# Compute consumer occupancy 
+occ <- vector()
+i=1
+for(E in seq){
+   occ[i] <- consumerOcc(resOpt=0.5, consOpt=0.5, E=E)
+   i = i + 1
+}
 
 # Plot
-plot(x=E, y=1-e/c, ylim=c(0,1), type='l',
+plot(x=seq, y=occ, ylim=c(0,1.1), xlim=c(0.5,1), type='l',
      lwd=lwd, yaxs="i", xaxs="i",
      yaxt='n', xaxt='n',
      cex.lab=2, cex.axis=1.5,
      ylab='', xlab='')
-lines(y=1-E-0.1, x=E, lwd=lwd, lty=2)
+lines(y=c(1,0), x=range(seq), lwd=lwd, lty=2)
 
 # Axis titles
 title(ylab = "Prevalence",
@@ -34,5 +40,3 @@ box(lwd=lwd)
 
 # Close file
 dev.off()
-
-
