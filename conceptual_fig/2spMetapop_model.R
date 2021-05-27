35e2bc9857773923374588acf721b3f5b994082d
vcameron1 <54558022+vcameron1@users.noreply.github.com>
2021-05-27 13:40:28 -0400

    update performance functions parameters

--- conceptual_fig/2spMetapop_model.R
+++ conceptual_fig/2spMetapop_model.R
@@ -23,16 +23,22 @@
 consumerOcc <- function(resOpt=0.5, consOpt=0.5, E=0.5){
   
   # Test
+  ## Normal distribution
   # E <- seq(0,1,le=100)
-  # plot(dnorm(E, mean = resOpt, sd = 0.1)/3, x=E) # Normal distribution
-  # points(Pc, x=E)
-  # points(dnorm(E, mean = consOpt, sd = 0.15)/3, x=E)
+  # plot(dnorm(E, mean = resOpt, sd = 0.4), x=E) 
+  # points(dnorm(E, mean = consOpt, sd = 0.5), x=E)
+  ## Quadratic function
+  #plot(1-3*(E-resOpt)^2, x=E, ylim=c(0,1))
+  #points(0.8-3*(E-consOpt)^2, x=E, ylim=c(0,1))
+
   
   # Resource prevalence (Pr=1-e/c)
-  Pr <- dnorm(E, mean = resOpt, sd = 0.5) 
+  Pr <- 0.8-3*(E-resOpt)^2 # Quadratic function
+    #dnorm(E, mean = resOpt, sd = 0.5) # Normal distribution
   
   # Consumer prevalence
-  Pc <- Pr-(1-dnorm(E, mean = consOpt, sd = 0.5)) 
+  Pc <- Pr - (1 - (0.8-3*(E-consOpt)^2)) # Quadratic function
+    #Pr-(1-dnorm(E, mean = consOpt, sd = 0.5)) # Normal distribution
   
   # return
   return <- c('Pconsumer' = Pc)
