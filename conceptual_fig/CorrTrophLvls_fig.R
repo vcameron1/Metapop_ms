b636b3b8c9b6150df22c781d8e2aaf2325bc0e8d
vcameron1 <54558022+vcameron1@users.noreply.github.com>
2021-05-31 17:26:20 -0400

    replace ggplot by image()

--- conceptual_fig/CorrTrophLvls_fig.R
+++ conceptual_fig/CorrTrophLvls_fig.R
@@ -15,38 +15,90 @@ lwd = 4
 png('../manuscript/img/concept_CorrTrophLvls.png', width = 150, height = 150, units='mm', res = 700)
 
 # Envrionmental gradient
-seq <- seq(0, 1, by=0.025)
+x <- y <- seq(0, 1, by=0.025)
 
-# Init data frame
-occupancy_df<- data.frame(resource=rep(seq, time=length(seq)), consumer=rep(seq, each=length(seq)), deltaOcc=0)
+z <- matrix(0, nrow = length(x), ncol = length(y))
 
 # Compute prevalences
-for(resource in occupancy_df$resource){
-  for(consumer in occupancy_df$consumer){
+i = 0
+for(resource in x){
+  i = i + 1
+  j = 0
+  for(consumer in y){
+    j = j + 1
     occ1 <- consumerOcc(resOpt=resource, consOpt=consumer, E=0.5) 
     occ2 <- consumerOcc(resOpt=resource, consOpt=consumer, E=0.6)
-    occupancy_df[occupancy_df$consumer==consumer & occupancy_df$resource==resource,'deltaOcc'] <- occ2 - occ1
+    z[j, i] <- occ2 - occ1
   }
 }
 
 # Plot
-ggplot(occupancy_df, aes(resource, consumer, fill= deltaOcc)) + 
-  xlab('Resource environmental optimum')+
-  ylab('Consumer environmental optimum')+
-  geom_tile() +
-  colorspace::scale_fill_continuous_diverging(p1=0.8, p2=1, l1=0.4) +
-  coord_fixed(expand = c(0, 0)) +
-  theme(axis.title=element_text(size=20),
-        axis.text.y = element_blank(),
-        axis.text.x = element_blank(),
-        axis.ticks = element_blank(),
-        panel.background = element_blank(),
-        panel.grid.major = element_blank(), 
-        panel.grid.minor = element_blank(),
-        panel.border = element_rect(colour = "black", fill=NA, size=lwd)) +
-  geom_abline(linetype = "dashed") +
-  geom_vline(xintercept = 0.5) +
-  geom_hline(yintercept = 0.5)
+par(pty = "s",par(pty = "s", mar=c(4.5,4.5,1,0)))
+lwd <- 4
+
+## Center range on 0
+max_abs <- max(abs(z))
+brk <- lattice::do.breaks(c(-max_abs, max_abs), length(c(z)))
+## Plot
+image(z, col=hcl.colors(length(z), "Red-Green"), breaks = brk,
+      lwd=lwd, yaxs="i", xaxs="i",
+      yaxt='n', xaxt='n',
+      cex.lab=2, cex.axis=1.5,
+      ylab='', xlab='')
+abline(0, 1, lty=2)
+box(lwd=lwd) 
+## Color scale
+par(mar=c(3,1,1,1))
+image.scale(z, col=hcl.colors(length(z), "Red-Green"))
+box()
 
 # Close file
 dev.off()
+
+
+
+
+
+
+
+#### Function to add a scale to the image() function ####
+
+# It is important to remember that for the scales to appear next to the plots, the layout must be correctly specified
+#layout(matrix(c(1,2,3,0,4,0), nrow=2, ncol=3), widths=c(4,4,1), heights=c(4,1))
+#layout.show(4)
+
+image.scale <- function(z, zlim, col = heat.colors(12),
+                        breaks, horiz=TRUE, ylim=NULL, xlim=NULL, ...){
+  if(!missing(breaks)){
+    if(length(breaks) != (length(col)+1)){stop("must have one more break than colour")}
+  }
+  if(missing(breaks) & !missing(zlim)){
+    breaks <- seq(zlim[1], zlim[2], length.out=(length(col)+1)) 
+  }
+  if(missing(breaks) & missing(zlim)){
+    zlim <- range(z, na.rm=TRUE)
+    zlim[2] <- zlim[2]+c(zlim[2]-zlim[1])*(1E-3)#adds a bit to the range in both directions
+    zlim[1] <- zlim[1]-c(zlim[2]-zlim[1])*(1E-3)
+    breaks <- seq(zlim[1], zlim[2], length.out=(length(col)+1))
+  }
+  poly <- vector(mode="list", length(col))
+  for(i in seq(poly)){
+    poly[[i]] <- c(breaks[i], breaks[i+1], breaks[i+1], breaks[i])
+  }
+  xaxt <- ifelse(horiz, "s", "n")
+  yaxt <- ifelse(horiz, "n", "s")
+  if(horiz){YLIM<-c(0,1); XLIM<-range(breaks)}
+  if(!horiz){YLIM<-range(breaks); XLIM<-c(0,1)}
+  if(missing(xlim)) xlim=XLIM
+  if(missing(ylim)) ylim=YLIM
+  plot(1,1,t="n",ylim=ylim, xlim=xlim, xaxt=xaxt, yaxt=yaxt, xaxs="i", yaxs="i", ...)  
+  for(i in seq(poly)){
+    if(horiz){
+      polygon(poly[[i]], c(0,0,1,1), col=col[i], border=NA)
+    }
+    if(!horiz){
+      polygon(c(0,0,1,1), poly[[i]], col=col[i], border=NA)
+    }
+  }
+}
+
