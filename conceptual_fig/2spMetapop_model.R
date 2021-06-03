86884451fa3fbf976a45047fc14a3eebd4a28f9d
vcameron1 <54558022+vcameron1@users.noreply.github.com>
2021-06-03 09:42:00 -0400

    updated params and parametered dashed (SDM) curve

--- conceptual_fig/2spMetapop_model.R
+++ conceptual_fig/2spMetapop_model.R
@@ -19,8 +19,9 @@
 # resOpt: Ressource environnemental optimum ([0,1])
 # consOpt: Consumer environnemental optimum ([0,1])
 # E: Environmental value at which to compute prevalence
+# consumer: logical, whether the consumer is dependent on the resource or not
 
-consumerOcc <- function(resOpt=0.5, consOpt=0.5, E=0.5){
+consumerOcc <- function(resOpt=0.5, consOpt=0.5, E=0.5, consumer=TRUE){
   
   # Test
   ## Normal distribution
@@ -28,16 +29,19 @@ consumerOcc <- function(resOpt=0.5, consOpt=0.5, E=0.5){
   # plot(dnorm(E, mean = resOpt, sd = 0.4), x=E) 
   # points(dnorm(E, mean = consOpt, sd = 0.5), x=E)
   ## Quadratic function
-  #plot(1-3*(E-resOpt)^2, x=E, ylim=c(0,1))
+  #plot(1-4*(E-resOpt)^2, x=E, ylim=c(0,1))
   #points(0.8-3*(E-consOpt)^2, x=E, ylim=c(0,1))
+  ## Absolute value function
+  #points(y=1 - 2*abs(E - consOpt), x=E)
 
   
   # Resource prevalence (Pr=1-e/c)
-  Pr <- 0.8-3*(E-resOpt)^2 # Quadratic function
+  Pr <- 1-4*(E-resOpt)^2 # Quadratic function
     #dnorm(E, mean = resOpt, sd = 0.5) # Normal distribution
   
   # Consumer prevalence
-  Pc <- Pr - (1 - (0.8-3*(E-consOpt)^2)) # Quadratic function
+  Pc <- if(consumer){ Pr - (1 - (1 - 2*abs(E - consOpt)))}else{ 1 - 2*abs(E - consOpt)} # Absolute value function
+    #Pr - (1 - (0.8-3*(E-consOpt)^2)) # Quadratic function
     #Pr-(1-dnorm(E, mean = consOpt, sd = 0.5)) # Normal distribution
   
   # return
