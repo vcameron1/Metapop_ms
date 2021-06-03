99b2dc3bdc4894f72a9a4d04aae7cdcd119e6761
vcameron1 <54558022+vcameron1@users.noreply.github.com>
2021-06-03 09:42:00 -0400

    updated params and parametered dashed (SDM) curve

--- conceptual_fig/metapopEffect_fig.R
+++ conceptual_fig/metapopEffect_fig.R
@@ -4,31 +4,37 @@
 # date: 26/05/2021
 ####
 
+# Dependencies
+source('./conceptual_fig/2spMetapop_model.R')
+
 # Save plot in file
-png('../manuscript/img/concept_metapopEffect.png', width = 150, height = 150, units='mm', res = 700)
+png('./manuscript/img/concept_metapopEffect.png', width = 150, height = 150, units='mm', res = 700)
 
 # Set graphic parameters
 par(pty = "s",par(pty = "s", mar=c(4.5,4.5,1,0)))
 lwd <- 4
 
 # Environmetal gradient
-seq <- seq(0.5, 1, le=20)
+time <- 20
+Env <- seq(0.5, 1, le=time)
 
-# Compute consumer occupancy 
-occ <- vector()
+# Compute consumer occ2upancy in 2 sp metapop model and single species model
+occ2 <- vector()
+occ1 <- vector()
 i=1
-for(E in seq){
-   occ[i] <- consumerOcc(resOpt=0.5, consOpt=0.5, E=E)
+for(E in Env){
+   occ2[i] <- consumerOcc(resOpt=0.5, consOpt=0.5, E=E)
+   occ1[i] <- consumerOcc(resOpt=0.5, consOpt=0.5, E=E, consumer=FALSE)
    i = i + 1
 }
 
 # Plot
-plot(x=seq, y=occ, ylim=c(0,1.1), xlim=c(0.5,1), type='l',
+plot(x=1:time, y=occ2, ylim=c(0,max(occ2)+.1), type='l',
      lwd=lwd, yaxs="i", xaxs="i",
      yaxt='n', xaxt='n',
      cex.lab=2, cex.axis=1.5,
      ylab='', xlab='')
-lines(y=c(1,0), x=range(seq), lwd=lwd, lty=2)
+lines(y=occ1, x=1:time, lwd=lwd, lty=2)
 
 # Axis titles
 title(ylab = "Prevalence",
