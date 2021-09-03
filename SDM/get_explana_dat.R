70c765f46df9bfd4dc07d45aa583a735bd4d7e4c
vcameron1 <54558022+vcameron1@users.noreply.github.com>
2021-09-03 15:40:53 -0400

    upload script to generate explana_dat

---
+++ SDM/get_explana_dat.R
@@ -0,0 +1,20 @@
+################
+# This script creates the explana_dat object
+# Victor Cameron
+# 02/08/21
+###############
+
+# Import data
+## Environmental
+climatePresent <- readRDS("./data_clean/bioclim_sQ.RDS")
+## Elevation
+elevation <- readRDS("./data_clean/elev_sQ.RDS")
+## Forest cover
+forestCover <- readRDS("./data_clean/forestCover_sQ.RDS")
+
+# Explana_dat
+explana_dat <- raster::stack(climatePresent, elevation, forestCover)
+names(explana_dat[[20]]) <- "elevation"
+
+# Save
+saveRDS(explana_dat, "./SDM/explana_dat.RDS")
