3cfe65669ba3283d9e18439e81555fa61fc52941
vcameron1 <54558022+vcameron1@users.noreply.github.com>
2021-09-16 09:16:44 -0400

    clean scripts

--- SDM/GRBI_SDM.R
+++ SDM/GRBI_SDM.R
@@ -8,6 +8,12 @@
 # The code in this script is adapted from Guillaume Blanchet work for the 2019 biodiversity modeling summer school
 
 
+# 0 - Load dependences ----------------------------------------------------
+
+
+source("./SDM/SDM_functions.R")
+
+
 # 1 - Load data -----------------------------------------------------------
 
 
@@ -16,6 +22,9 @@ GRBI_points <- readRDS("./data_clean/GRBI_rasterPoints_sQ.RDS")
 
 # Explanatory variables
 explana_dat <- readRDS("./SDM/explana_dat.RDS")
+## Square temperature
+explana_dat[["mat2"]] <- explana_dat[["mat"]]
+raster::values(explana_dat[["mat2"]])<- raster::values(explana_dat[["mat"]])^2
 
 # Polygon of the region boundaries
 spacePoly <- readRDS("./SDM/spacePoly.RDS")
@@ -28,15 +37,15 @@ names(explana_dat)
 dev.new()
 
 # Rasterize GRBI occurences
-GRBI_r <- raster::rasterize(raster::coordinates(GRBI_points)[,1:2], explana_dat[["bio1"]], fun='count', background=0)
+GRBI_r <- raster::rasterize(raster::coordinates(GRBI_points)[,1:2], explana_dat[["mat"]], fun='count', background=0)
 
-# bio1 : mean annual temperature
-hist(raster::values(explana_dat[["bio1"]])) 
-boxplot(raster::values(explana_dat[["bio1"]]) ~ raster::values(GRBI_r))
+# mat : mean annual temperature
+hist(raster::values(explana_dat[["mat"]])) 
+boxplot(raster::values(explana_dat[["mat"]]) ~ raster::values(GRBI_r))
 
-# bio12 : mean annual precipitations
-hist(raster::values(explana_dat[["bio12"]])) 
-boxplot(raster::values(explana_dat[["bio12"]]) ~ raster::values(GRBI_r))
+# tap : mean total annual precipitations
+hist(raster::values(explana_dat[["tap"]])) 
+boxplot(raster::values(explana_dat[["tap"]]) ~ raster::values(GRBI_r))
 
 # elevation
 hist(raster::values(explana_dat[["elevation"]]))
@@ -103,7 +112,7 @@ for(i in 1:3){
 }
 
 
-# 4 - Model selection -----------------------------------------------------
+# 4 - Model ---------------------------------------------------------------
 
 ##################
 # Habitat requirements of GRBI accoring to COSEWIC
@@ -118,57 +127,15 @@ for(i in 1:3){
 ##################
 
 ##################
-# Hypotheses
-## 1. T, P, elevation, and forest cover are important : "bio1 + bio12 + elevation + type_couv + cl_dens + cl_haut"
-## 2. T, P, elevation, and forest cover are important and forest cover interacts with elevation : "bio1 + bio12 + elevation * type_couv * cl_dens * cl_haut"
+# Hypothesys
 ## 3. T, P, elevation, and forest cover are important and climate interacts with elevation : "bio1 * bio12 * elevation + type_couv + cl_dens + cl_haut"
-## 4. T, P, elevation, and forest cover are important and elevation interacts with climate and forest cover variables interact : "bio1 * bio12 * elevation + type_couv * cl_dens * cl_haut"
 ##################
 
 # Model fomula
-cov <- list(c("bio1^2 + bio12 + elevation + type_couv + cl_dens + cl_haut"),
-            c("bio1^2 + bio12 + elevation * type_couv * cl_dens * cl_haut"),
-            c("bio1^2 * bio12 * elevation + type_couv + cl_dens + cl_haut"),
-            c("bio1^2 * bio12 * elevation + type_couv * cl_dens * cl_haut"),
-            c("bio1^2 * bio12 * elevation + type_couv * cl_dens + cl_haut"))
+cov <- c("mat * mat2 * tap * elevation + type_couv + cl_dens + cl_haut") 
 
-aic = rep(NA, length(cov))
-
-for(i in 5:length(cov)){
-  # Downweighted poisson regression (point process model)
-  res <- SDM.glm(spacePoly=spacePoly, 
-                  GRBI_points=GRBI_points,
-                  covariables = cov, 
-                  pred = explana_dat,
-                  nquad = 1000000,         
-                  quadOverlay = TRUE,
-                  nquadWanted = FALSE)
-
-  aic[i] <- res[["model"]]$aic
-}
-
-plot(1:length(cov), aic, type = "o")
-(covariables <- cov[which.min(aic)])
-
-
-# Summary
-model <- res[["model"]]
-summary(model)
-# AUC and plot
-SDM.AUC(model, newdata=explana_dat, GRBI_points=GRBI_points, RL_cutoff = 0.05, plot_prediction = TRUE)
-# Plot predictions
-SDM.plot(model, newdata = explana_dat, logPred = TRUE, GRBI_points, points = TRUE, main = "GLM prediction (log)")
-# Estimated number of quadrature point needed
-mod[["nquadWanted"]]
-
-
-# 5 - Climate warming projection ------------------------------------------
-
-
-# Saturated model
-  # # Downweighted poisson regression (point process model)
-  cov <- c("bio1 * bio12 * elevation + type_couv + cl_dens + cl_haut")
-  res <- SDM.glm(spacePoly=spacePoly, 
+# Downweighted poisson regression (point process model)
+res <- SDM.glm(spacePoly=spacePoly, 
                   GRBI_points=GRBI_points,
                   covariables = cov, 
                   pred = explana_dat,
@@ -177,171 +144,52 @@ mod[["nquadWanted"]]
                   nquadWanted = FALSE)
 model <- res[["model"]]
 summary(model)
+
 # Plot predictions
 SDM.plot(model, newdata = explana_dat, logPred = TRUE, GRBI_points, points = FALSE, main = "GLM prediction (log)")
 # Predictions for Eastern Townships
 SDM.plot(model, newdata = explana_dat, logPred = TRUE, GRBI_points, points = FALSE, main = "GLM prediction EasternTownships (log)", xlim=c(-73,-70), ylim=c(45,46))
+SDM.AUC(model, newdata=explana_dat, GRBI_points=GRBI_points, RL_cutoff = 0.05, plot_prediction = FALSE)
 
-#### + 2°C ####
-explana_warm2 <- explana_dat
-raster::values(explana_warm2[["bio1"]]) <- raster::values(explana_warm2[["bio1"]]) + 2
-# Plot predictions
-SDM.plot(model, newdata = explana_warm2, logPred = TRUE, GRBI_points, points = FALSE, main = "GLM prediction +2°C")
-# Predictions for Eastern Townships
-SDM.plot(model, newdata = explana_warm2, logPred = TRUE, GRBI_points, points = FALSE, main = "GLM prediction +2°C EasternTownships (log)", xlim=c(-73,-70), ylim=c(45,46))
 
-#### + 4°C ####
-explana_warm4 <- explana_dat
-raster::values(explana_warm4[["bio1"]]) <- raster::values(explana_warm4[["bio1"]]) + 4
-# Plot predictions
-SDM.plot(model, newdata = explana_warm4, logPred = TRUE, GRBI_points, points = FALSE, main = "GLM prediction +4°C")
-SDM.plot(model, newdata = explana_warm4, logPred = TRUE, GRBI_points, points = FALSE, main = "GLM prediction +4°C EasternTownships (log)", xlim=c(-73,-70), ylim=c(45,46))
+# 6 - Test model ----------------------------------------------------------
+
+
+# Temperature gradient
+mat <- seq(from=min(raster::values(explana_dat[["mat"]]), na.rm = TRUE), to=max(raster::values(explana_dat[["mat"]]), na.rm = TRUE), length.out=100)
+mat2 <- mat^2
+
+# Fix other variables
+dat <- as.data.frame(raster::values(explana_dat))[1:100,]
+dat$tap <- mean(raster::values(explana_dat[["tap"]]), na.rm = TRUE)
+dat$type_couv <- 1
+dat$cl_dens <- 1
+dat$cl_haut <- 7
+dat$mat <- mat
+dat$mat2 <- mat2
+elev <- seq(500, 1200, by = 25)
+
+# Predict
+intensity <- list()
+for(i in 1:length(elev)){
+  dat$elevation <- elev[i]
+  intensity[[i]] <- predict(model, newdata = dat, type = "response")
+}
+
+# Cols
+colo <- colorRampPalette(c("grey90", "steelblue4", 
+                          "steelblue2", "steelblue1", 
+                          "gold", "red1", "red4"))(length(elev))
+
+# Plot prediction
+plot(y = log(intensity[[length(elev)]]), x = dat$mat, col = colo[length(elev)],ylab="log(intensity)", type="l",
+    xlab = "Temperature (°C)")
+for(i in 1:(length(elev)-1)){
+  lines(y = log(intensity[[i]]), x = dat$mat, col=colo[i])
+}
+legend(3.6, 14, legend = elev, fill = colo, ncol = 2, title = "Elevation (m)", bty = 'n')
 
 
 # 6 - Functions -----------------------------------------------------------
 
-
-#==========================
-# Function to run non 
-# spatial SDMs 
-#==========================
-
-SDM.glm <- function(spacePoly, GRBI_points, covariables = ".", pred, nquad = 10000, quadOverlay = TRUE, nquadWanted = FALSE){
-  # quadOverlay if true, quadrature points are randomly placed, if false, those overlaying GRBI points are excluded
-    
-    # Find the area of the survey region
-    areaRegion <- raster::area(spacePoly)/1000000
-
-    # Define random samples
-    set.seed(12)
-    xSmpl <- runif(5000000, raster::xmin(spacePoly), raster::xmax(spacePoly))
-    ySmpl <- runif(5000000, raster::ymin(spacePoly), raster::ymax(spacePoly))
-    
-    # Organise them into a SpatialPoints object
-    xySmpl <- sp::SpatialPoints(cbind(xSmpl, ySmpl), proj4string = spacePoly@proj4string)
-    ## Reject those where there are GRBI points
-    if(!quadOverlay){
-      xySmpl_raster <- raster::rasterize(raster::coordinates(xySmpl)[,1:2], pred[[1]], fun='count', background=0)
-      xySmpl_raster[xySmpl_raster>=1] <- 1
-      GRBI_raster <- raster::rasterize(raster::coordinates(GRBI_points)[,1:2], pred[[1]], fun='count', background=0)
-      xySmpl_raster[GRBI_raster > 0] <- 0
-      xySmpl <- sp::SpatialPoints(xySmpl_raster, proj4string = spacePoly@proj4string)
-    }
-
-    # Construct a SpatialPolygons object
-    landPoly <- maptools::unionSpatialPolygons(spacePoly, ID = rep(1,length(spacePoly)))
-    
-    # Find which one is in landPoly
-    quadIntersect <- raster::intersect(xySmpl, landPoly) 
-    
-    # Select nquad of the selected points
-    quadSel <- raster::coordinates(quadIntersect)[1:nquad,]
-    
-    # Organise coordinates
-    spQuadxy <- rbind(raster::coordinates(GRBI_points)[,1:2], quadSel)
-    
-    # Extract variables
-    datSpQuad <- raster::extract(pred, raster::coordinates(spQuadxy), method = "simple")
-    datSpQuad <- as.data.frame(datSpQuad)
-    
-    # Build response variable
-    spQuad <- c(rep(1, length(GRBI_points)), rep(0, nquad))
-
-    # Build weight
-    Weight <- rep(1/nquad, length(spQuad)) 
-    Weight[spQuad == 0] <- areaRegion / nquad
-
-    # Point process model
-    model <- glm(formula(paste("spQuad/Weight ~ ", covariables)),
-                data = datSpQuad, 
-                family = poisson(), 
-                weights = Weight) 
-
-    # Check if enough quadrature points were used
-    # # Calculate the estimated intensity at the quadrature points
-    if(nquadWanted){
-        predQuad <- datSpQuad[-(1:length(GRBI_points)),]
-        intensityQuad <- predict(model,
-                                newdata = predQuad,
-                                type = "response")
-        sdIntensityQuad <- sd(intensityQuad, na.rm=TRUE)
-        nquadWanted <- areaRegion^2 * sdIntensityQuad^2 / qnorm(0.975)
-    }else{
-        nquadWanted <- NULL
-    }
-    
-
-    return(list("model" = model, y = spQuad/Weight, weights = Weight, nquadWanted = nquadWanted))
-}
-
-
-#==========================
-# Function to plot non 
-# spatial SDMs 
-#==========================
-
-SDM.plot <- function(model, newdata, logPred = TRUE, GRBI_points, points = TRUE, ...){
-
-
-    dat <- as.data.frame(raster::values(newdata))
-    if(length(names(newdata)) == 1) names(dat) <- "datSpQuad"
-    intensityMap <- predict(model,
-                            newdata = dat,
-                            type = "response")
-    
-    # Build the raster objects
-    prediction <- raster::raster(newdata) 
-    raster::values(prediction) <- intensityMap
-
-    # Draw the map
-    colo <- colorRampPalette(c("grey90", "steelblue4", 
-                          "steelblue2", "steelblue1", 
-                          "gold", "red1", "red4"))(200)
-
-    dev.new()
-    if(!logPred){
-      raster::plot(prediction, col = colo,  zlim = c(0, max(raster::values(prediction), na.rm = TRUE)), axes = FALSE, box = FALSE, ...)
-    }else{
-      prediction[prediction < 0.01] <- 0.01
-      logpred <- log(prediction)
-      logpred[prediction == -Inf] <- 0
-      raster::plot(logpred, col = colo,  zlim = c(min(raster::values(logpred), na.rm = TRUE), max(raster::values(logpred), na.rm = TRUE)), axes = FALSE, box = FALSE, ...)
-      print("Intensity predictions were log-transformed")
-    }
-    
-    if(points) points(raster::coordinates(GRBI_points), pch = 3, cex = 0.05)
-
-}
-
-
-#==========================
-# Function to compute GLM model AUC
-#==========================
-
-SDM.AUC <- function(model, newdata, GRBI_points, RL_cutoff, plot_prediction = FALSE, points = TRUE,...){
-  
-  # Projection of the model from new data
-  dat <- as.data.frame(raster::values(newdata))
-  if(length(names(newdata)) == 1) names(dat) <- "datSpQuad"
-  pred <- predict(model, newdata = dat, type = "response")
-
-  # GRBI presence raster
-  GRBI_raster <- raster::rasterize(raster::coordinates(GRBI_points)[,1:2], newdata[[1]], fun='count', background=0)  
-
-  # Projected distribution raster
-  pred[pred >= RL_cutoff] <- 1
-  pred[pred < RL_cutoff] <- 0
-  r <- newdata
-  raster::values(r) <- pred
-  
-  # Plot prediction
-  if(plot_prediction){
-    dev.new()
-    raster::plot(r[[1]], axes = FALSE, box = FALSE, ...) 
-    if(points) points(GRBI_points, pch=19, cex=0.02)
-  }
-
-  # Compute AUC 
-  pROC::auc(raster::values(GRBI_raster), pred) # Observed vs predicted (GRBI = raster values)
-}
 
