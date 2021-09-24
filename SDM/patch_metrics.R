2e878d9cdd0ea70b77487197aa5ed2d6773f5e9c
vcameron1 <54558022+vcameron1@users.noreply.github.com>
2021-09-24 10:42:15 -0400

    upload script to compute patch metrics

--- SDM/patch_metrics.R
+++ SDM/patch_metrics.R
@@ -1,45 +1,52 @@
 # taille des patchs et distance
 
-# Params
-RL_cutoff <- log(0.05)
-
 patch.metrics <- function(raster, RL_cutoff = 0.05){
 
-    #### Compute total habitat size ####
-    raster[raster < RL_cutoff] <- NA
-    area <- raster::area(raster, na.rm=TRUE) # in km2
-    area <- area[!is.na(area)]
-    sumArea <- sum(area, na.rm = TRUE)
+    # 
+    RL_cutoff = log(RL_cutoff)
 
-    # Identify groups of cells that are connected
-    clump <- raster::clump(raster >= RL_cutoff)
+    # Check if any values excced RL_cutoff
+    if(maxValue(raster) < RL_cutoff){
+        sumArea = 0
+        n = 0
+        patchArea = 0
+        d_ij = NA
+    }else{
+        #### Compute total habitat size ####
+        raster[raster < RL_cutoff] <- NA
+        area <- raster::area(raster, na.rm=TRUE) # in km2
+        area <- area[!is.na(area)]
+        sumArea <- sum(area, na.rm = TRUE)
+
+        # Identify groups of cells that are connected
+        clump <- raster::clump(raster >= RL_cutoff)
 
 
-    #### Compute patch area (m2) ####
-    values <- unique(values(clump))
-    values <- values[!is.na(values)]
-    patchArea <- data.frame(patch = values, area = 0)
-    for(patch in patchArea$patch){
-        # # Compute clump area 
-        clumpX <- clump
-        clumpX[clumpX != patch] <- NA
-        clumpArea <- raster::area(clumpX, na.rm = TRUE)
-        clumpArea <- clumpArea[!is.na(clumpArea)]
-        clumpArea <- sum(clumpArea, na.rm = TRUE)
-            
-        # # Save in df
-        patchArea[which(patchArea$patch == patch), 'area'] <- clumpArea
+        #### Compute patch area (m2) ####
+        values <- unique(values(clump))
+        values <- values[!is.na(values)]
+        patchArea <- data.frame(patch = values, area = 0)
+        for(patch in patchArea$patch){
+            # # Compute clump area 
+            clumpX <- clump
+            clumpX[clumpX != patch] <- NA
+            clumpArea <- raster::area(clumpX, na.rm = TRUE)
+            clumpArea <- clumpArea[!is.na(clumpArea)]
+            clumpArea <- sum(clumpArea, na.rm = TRUE)
+                
+            # # Save in df
+            patchArea[which(patchArea$patch == patch), 'area'] <- clumpArea
+        }
+
+
+        #### Number of patches ####
+        n = length(patchArea$patch)
+
+
+        #### Inter-patch distance ####
+        clumpPoly <- raster::rasterToPolygons(clump, dissolve = TRUE)
+        d_ij <- rgeos::gDistance(clumpPoly, byid = TRUE)
     }
-
-
-    #### Number of patches ####
-    n = length(patchArea$patch)
-
-
-    #### Inter-patch distance ####
-    clumpPoly <- raster::rasterToPolygons(clump, dissolve = TRUE)
-    d_ij <- rgeos::gDistance(clumpPoly, byid = TRUE)
-
 
     #### Return ####
     return(
@@ -47,7 +54,6 @@ patch.metrics <- function(raster, RL_cutoff = 0.05){
         totalArea = sumArea,
         n = n,
         patchArea = patchArea,
-        d_ij = d_ij,
-        )
+        d_ij = d_ij)
     )
 }
