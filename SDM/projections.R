3c8b53a1a53215a9dca9b0a4fffe56017da378aa
vcameron1 <54558022+vcameron1@users.noreply.github.com>
2021-09-24 10:41:35 -0400

    add script to run climate warming projections

--- SDM/projections.R
+++ SDM/projections.R
@@ -1,6 +1,9 @@
 # Climate warming projections
 
 
+# 1 - Projections ---------------------------------------------------------
+
+
 # Build predictions for +4°C
 cc <- seq(0, 4, by = 0.2)
 for(i in seq_along(cc)){
@@ -31,40 +34,79 @@ for(i in seq_along(cc)){
     }else{
       SDM_GRBI <- raster::addLayer(SDM_GRBI, logpred)
     }
+
+  # # Name layer
+  names(SDM_GRBI)[[i]] <- paste0(cc[i])
 }
 
-# Plot climate warming as a gif
-colo <- colorRampPalette(c("grey90", "steelblue4", 
+# Save projections
+saveRDS(SDM_GRBI, "./SDM/results/projections_GRBI.RDS")
+
+
+# 2 - Patch metrics -------------------------------------------------------
+
+
+# Dependencies
+source("./SDM/patch_metrics.R")
+
+# Wrapper to use patch.metrics function over all temperature predictions
+get.metrics <- function(rasterPred, RL_cutoff = 0.05, cc = cc){
+  metrics_list <- list()
+  for(i in seq_along(cc)){
+  metrics <- patch.metrics(rasterPred[[i]], RL_cutoff = RL_cutoff)
+  metrics_list[[i]] <- metrics
+  }
+  return(metrics_list)
+}
+
+# Compute patch metrics over full distribution
+# # Limit analysis to current distribution
+e <- raster::extent(c(xmin = -75, xmax = -64, ymin = 45, ymax = 49.5))
+SDM_QC <- raster::crop(SDM_GRBI, e)
+metrics_QC <- get.metrics(SDM_QC, RL_cutoff = 0.05, cc = cc)
+#saveRDS(metrics_QC, "./SDM/results/metrics_QC.RDS")
+
+# Patch metrics over EasternTownships
+e_ET <- raster::extent(c(xmin = -73, xmax = -70, ymin = 45, ymax = 46))
+SDM_ET <- raster::crop(SDM_GRBI, e_ET)
+metrics_ET <- get.metrics(SDM_ET, RL_cutoff = 0.05, cc = cc)
+#saveRDS(metrics_ET, "./SDM/results/metrics_ET.RDS")
+
+# Patch metrics over forêt Montmorency
+e_M <- raster::extent(c(xmin = -72.2, xmax = -70, ymin = 46.8, ymax = 48.2))
+SDM_M <- raster::crop(SDM_GRBI, e_M)
+metrics_M <- get.metrics(SDM_M, RL_cutoff = 0.05, cc = cc)
+#saveRDS(metrics_M, "./SDM/results/metrics_M.RDS")
+
+
+# 3 - Plot predictions ----------------------------------------------------
+
+
+# Function to plot climate warming as a gif
+library(animation)
+plot.gif <- function(raster, file.name, xlim, ylim,
+                     frames.interval = 0.05, zlim = c(-5,10), ...){
+  colo <- colorRampPalette(c("grey90", "steelblue4", 
                           "steelblue2", "steelblue1", 
                           "gold", "red1", "red4"))(200)
-par(mar=c(0,0,0,0))
-library(animation)
-ani.options(interval=.05)
-saveGIF({
-  for (i in 1:raster::nlayers(SDM_GRBI)){
-    raster::plot(SDM_GRBI[[i]], col = colo, maxpixels = raster::ncell(SDM_GRBI),  zlim = c(-5,10), axes = FALSE, box = FALSE, legend = FALSE, main = "")
-  }
-}, 
-movie.name = "./SDM/results/GRBI.gif") 
+  par(mar=c(0,0,0,0))
+  ani.options(interval = frames.interval)
+  saveGIF({
+    for (i in 1:raster::nlayers(raster)){
+      raster::plot(raster[[i]], col = colo, maxpixels = raster::ncell(raster),  
+                   zlim = zlim, axes = FALSE, box = FALSE, legend = FALSE, 
+                   xlim = xlim, ylim = ylim, ...)
+    }
+  }, 
+  movie.name = file.name) 
+}
 
 # Gif for EasternTownships
-par(mar=c(0,0,0,0))
-saveGIF({
-  for (i in 1:raster::nlayers(SDM_GRBI)){
-    raster::plot(SDM_GRBI[[i]], col = colo, maxpixels = raster::ncell(SDM_GRBI),  zlim = c(-5,10), axes = FALSE, box = FALSE, legend = FALSE, main = "",
-    xlim=c(-73,-70), ylim=c(45,46))
-  }
-}, 
-movie.name = "./SDM/results/GRBI_ET.gif") 
-
+plot.gif(SDM_GRBI, file.name = "./SDM/results/GRBI_ET.gif", xlim=c(-73,-70), ylim=c(45,46),
+        frames.interval = 0.05, zlim = c(-5,10), main = "")
 
 # Gif for Forêt Montmorency
-par(mar=c(0,0,0,0))
-saveGIF({
-  for (i in 1:raster::nlayers(SDM_GRBI)){
-    raster::plot(SDM_GRBI[[i]], col = colo, maxpixels = raster::ncell(SDM_GRBI),  zlim = c(-5,10), axes = FALSE, box = FALSE, legend = FALSE, main = "",
-    xlim=c(-72.2,-70), ylim=c(46.8,48.2))
-  }
-}, 
-movie.name = "./SDM/results/GRBI_Montmo.gif") 
+plot.gif(SDM_GRBI, file.name = "./SDM/results/GRBI_Montmo.gif", xlim=c(-72.2,-70), ylim=c(46.8,48.2), 
+         frames.interval = 0.05, zlim = c(-5,10), main = "")
+
 
