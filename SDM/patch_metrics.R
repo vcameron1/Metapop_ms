a5b8304f8ae3e7200be6def3ea35917c57ccc1b2
vcameron1 <54558022+vcameron1@users.noreply.github.com>
2021-10-11 13:35:42 -0400

    add function to compute capacity

--- SDM/patch_metrics.R
+++ SDM/patch_metrics.R
@@ -83,3 +83,41 @@ patch.metrics <- function(raster, RL_cutoff = 0.05){
         capacity = eigen)
     )
 }
+
+
+# This function computes the capacity of a landscape
+## metrics <- readRDS("./SDM/results/metrics_ET.RDS")
+capacity <- function(metrics, alpha = 1/2){
+    
+    eigen <- c()
+    
+    # Loop through time steps
+    for(i in 1:length(metrics)){
+
+        if(metrics[[i]]$n == 0){
+            eigen[i] = 0
+            next
+        }
+
+        #### Extract patch area ####
+        area <- metrics[[i]]$patchArea
+
+        #### Extract inter-patch distance ####
+        dist <- metrics[[i]]$d_ij
+
+        #### Metapop capacity ####
+        ##exp(-a*d)*A1*A2 as in Hanski 2000
+        land <- matrix(rep(0, length(dist)), nrow=nrow(dist))
+        for(col in area$patch){
+            for(row in area$patch){
+                if(col == row) next
+                land[row,col] <- exp(-alpha*dist[row,col]) * area[col,'area'] * area[row,'area']
+            }
+        }
+
+        # Metapop capacity
+        eigen[i] = eigen(land)$values[1]
+
+    }
+    return(eigen)
+}
